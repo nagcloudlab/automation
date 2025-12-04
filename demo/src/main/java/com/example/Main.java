6eb19a697658a1b2b226d89f248455e7cec22c52
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-12-04 13:15:59 +0530

    play-with-maven

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition: " + calculator.add(a, b));
+    }
+}
